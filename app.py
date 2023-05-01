9649ae7a7bd4edd80c3ce34c940966ab40b219dd
rashi2911 <rashi29.agarwal@gmail.com>
2023-05-01 21:59:45 +0530

    update app.py

--- app.py
+++ app.py
@@ -259,4 +259,4 @@ def treatment():
 
 
 if __name__=='__main__':
-    app.run(debug=True, host="127.0.0.1", port=5000,threaded=True)
+    app.run(debug=True, host="0.0.0.0", port=5000,threaded=True)
